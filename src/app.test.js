eb8e51722774b0ff930f0f02d0fc0ce8cc8feb36
MasterNay <155442296+MasterNay@users.noreply.github.com>
2025-04-24 23:44:17 +0700

    fix test

--- src/app.test.js
+++ src/app.test.js
@@ -1,6 +1,6 @@
 //app.test.js
 const { dayOfTheWeek } = require('./app');
-
+const { monthOfTheYear } = require('./app');
 
 test('getDay returns the long-format day of the week', () => {
     const day = dayOfTheWeek( new Date('3/11/2020') );
